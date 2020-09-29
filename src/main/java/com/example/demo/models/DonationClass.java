387388adc87c91b7166bdfa92f242f2a1af2e8da
christina bartholomæussen <chri782z@edu.ucl.dk>
2020-09-29 11:33:35 +0200

    add donation classes

---
+++ src/main/java/com/example/demo/models/DonationClass.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+public class DonationClass {
+
+    private int amount;
+
+    public DonationClass(){}
+
+    public int getAmount() {
+        return amount;
+    }
+}
