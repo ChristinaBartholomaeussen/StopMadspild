312eb39bcb32e9dabab6c24df3e39f09cd1ad1c7
GenuinelyMeizner <frederikmpetersen@gmail.com>
2020-09-30 14:05:25 +0200

    CHRISTINA FILENOTFOUND DEBUG

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Controller
