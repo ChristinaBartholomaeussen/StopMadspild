a6adaa59bd4af2d2d297d6ce80c87f6fab6baaf0
GenuinelyMeizner <frederikmpetersen@gmail.com>
2020-09-30 12:37:11 +0200

    Insert CO2Calc into project - needs html/css

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.BeregnerModel;
 import com.example.demo.models.DonationClass;
+import com.example.demo.services.BeregnerService;
 import com.example.demo.services.MyDonation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -53,6 +55,33 @@ public class IndexController
     {
         return "omOrganisationen";
     }
+
+    //Beregner start
+    BeregnerModel beregnerModel = new BeregnerModel();
+
+    BeregnerService beregnerService = new BeregnerService();
+
+    @GetMapping ("/calc")
+    public String calc(Model calcModel) {
+
+        calcModel.addAttribute("beregnerModel", beregnerModel);
+
+        return "beregner";
+    }
+
+    @PostMapping("/postCalc")
+    public String postCalc(WebRequest dataFromForm) {
+
+        double value = Double.valueOf(dataFromForm.getParameter("output"));
+
+        BeregnerModel calcFromForm = new BeregnerModel(beregnerService.calc(value));
+
+        beregnerModel = calcFromForm;
+
+        return "redirect:/calc";
+    } //Beregner slut
 }
+
+
 
 
